100a85e71567d00365a9bea814f138bf6a7d6c31
aurrajaime-sudo <aurra.jaime@gmail.com>
2025-10-29 00:42:29 -0300

    Fix import statement

--- src/components/forms/ClientForm.tsx
+++ src/components/forms/ClientForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Bolt Database } from '../../lib/Bolt Database';
+import { BoltDatabase } from '../../lib/Bolt Database';
 import { Plus, Trash2, Building2, MapPin, Phone, Mail, X, Key, Eye, EyeOff, Copy } from 'lucide-react';
 
 interface ElevatorData {
